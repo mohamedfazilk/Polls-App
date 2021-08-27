183fc184217ad6c07bacfa564839f669cfa9428c
mohamedfazilk <mohamedfazil961@gmail.com>
2021-08-27 16:05:36 +0530

    adding more views

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("hello, Your polls application")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s" % question_id)
+
+def results(request, question_id):
+    response = "You are looking the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
