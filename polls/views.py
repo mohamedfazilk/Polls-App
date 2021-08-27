c598cd5e326627589a8037e38996791dabc494d0
mohamedfazilk <mohamedfazil961@gmail.com>
2021-08-27 16:39:59 +0530

    Http404 raises if a question  requested ID doesn’t exist in detail view.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from . models import  Question
 from django.template import loader
 from django.shortcuts import render
@@ -13,7 +13,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s" % question_id)
+   try:
+       question = Question.objects.get(pk=question_id)
+   except Question.DoesNotExist:
+       raise Http404("Question does not exist")
+   return  render(request, 'polls/detail.html', {'question':question})
+
 
 def results(request, question_id):
     response = "You are looking the results of question %s"
