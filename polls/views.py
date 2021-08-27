b5c59163a1b427893dd094b26a79b7f045a90049
mohamedfazilk <mohamedfazil961@gmail.com>
2021-08-27 16:31:21 +0530

    replace Httpresponse by render

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse,
+from django.http import HttpResponse
 from . models import  Question
 from django.template import loader
+from django.shortcuts import render
 
 def index(request):
     latest_question_list =Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {'latest_question_list':latest_question_list}
-    return HttpResponse(template.render(request, context))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
